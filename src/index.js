dd53c9a42380fa9a4a68f34d1ea10ad6b9dfcd34
Kirill <lika_208@mail.ru>
2022-09-24 21:28:08 +0300

    testCommit

--- src/index.js
+++ src/index.js
@@ -7,12 +7,7 @@ import {BrowserRouter} from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
-export default function TokenTableComponent({items=[  {id: 1, name: 'Pyshky.net', status: 'green', type: 'TRST', conditions: 'x2,6 months', volume: 120000, roi: 4, free: 20, hedge: 20},
-{id: 1412, name: 'Web3 P2P University', status: 'green', type: 'TRST', conditions: 'x2,1 years', volume: -100, roi: 6, free: 1, hedge: 0},
-{id: 2, name: 'NFT-Flowershop', status: 'yellow', type: 'THT', conditions: 'x4,2 years', volume: 80000, roi: 23, free: 12, hedge: 0},
-{id: 4, name: 'Web3 P2P University', status: 'red', type: 'TRST', conditions: 'x2,1 years', volume: 200000, roi: 6, free: 1, hedge: 0},
-{id: 14, name: 'Web3 P2P University', status: 'red', type: 'TRST', conditions: 'x2,1 years', volume: 100, roi: 6, free: 1, hedge: 0},
-], onFilter=(filterItems) => console.log(filterItems),   onBuy=()=>{}, filters={
+export default function TokenTableComponent({items=[], onFilter=(filterItems) => console.log(filterItems),   onBuy=()=>{}, filters={
   status: null, 
   type: null
 }}) {
